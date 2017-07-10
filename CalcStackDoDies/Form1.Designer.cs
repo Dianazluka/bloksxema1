5d1fb76c56ecedbf0b326977c199540ebec90dd1
Sabrae <sabraerillintar@gmail.com>
2017-07-10 15:49:35 +0300

    Fist version of Calc

--- CalcStackDoDies/Form1.Designer.cs
+++ CalcStackDoDies/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Plus = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Div = new System.Windows.Forms.Button();
+            this.Mul = new System.Windows.Forms.Button();
+            this.Input2 = new System.Windows.Forms.TextBox();
+            this.Input1 = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Plus
+            // 
+            this.Plus.Location = new System.Drawing.Point(34, 140);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(75, 23);
+            this.Plus.TabIndex = 0;
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = true;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // Minus
+            // 
+            this.Minus.Location = new System.Drawing.Point(148, 140);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(75, 23);
+            this.Minus.TabIndex = 1;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = true;
+            this.Minus.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Div
+            // 
+            this.Div.Location = new System.Drawing.Point(148, 190);
+            this.Div.Name = "Div";
+            this.Div.Size = new System.Drawing.Size(75, 23);
+            this.Div.TabIndex = 2;
+            this.Div.Text = "/";
+            this.Div.UseVisualStyleBackColor = true;
+            this.Div.Click += new System.EventHandler(this.Div_Click);
+            // 
+            // Mul
+            // 
+            this.Mul.Location = new System.Drawing.Point(34, 190);
+            this.Mul.Name = "Mul";
+            this.Mul.Size = new System.Drawing.Size(75, 23);
+            this.Mul.TabIndex = 3;
+            this.Mul.Text = "*";
+            this.Mul.UseVisualStyleBackColor = true;
+            this.Mul.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Input2
+            // 
+            this.Input2.Location = new System.Drawing.Point(34, 60);
+            this.Input2.Name = "Input2";
+            this.Input2.Size = new System.Drawing.Size(189, 20);
+            this.Input2.TabIndex = 4;
+            // 
+            // Input1
+            // 
+            this.Input1.Location = new System.Drawing.Point(34, 34);
+            this.Input1.Name = "Input1";
+            this.Input1.Size = new System.Drawing.Size(189, 20);
+            this.Input1.TabIndex = 5;
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(34, 86);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(189, 20);
+            this.Result.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Input1);
+            this.Controls.Add(this.Input2);
+            this.Controls.Add(this.Mul);
+            this.Controls.Add(this.Div);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Plus);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Div;
+        private System.Windows.Forms.Button Mul;
+        private System.Windows.Forms.TextBox Input2;
+        private System.Windows.Forms.TextBox Input1;
+        private System.Windows.Forms.TextBox Result;
     }
 }
 
--- CalcStackDoDies/Form1.cs
+++ CalcStackDoDies/Form1.cs
@@ -16,5 +16,37 @@ namespace CalcStackDoDies
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(Input1.Text);
+            double second = Convert.ToDouble(Input2.Text);
+            double result = first - second;
+            Result.Text = Convert.ToString(result);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(Input1.Text);
+            double second = Convert.ToDouble(Input2.Text);
+            double result = first * second;
+            Result.Text = Convert.ToString(result);
+        }
+
+        private void Plus_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(Input1.Text);
+            double second = Convert.ToDouble(Input2.Text);
+            double result = first + second;
+            Result.Text = Convert.ToString(result);
+        }
+
+        private void Div_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(Input1.Text);
+            double second = Convert.ToDouble(Input2.Text);
+            double result = first / second;
+            Result.Text = Convert.ToString(result);
+        }
     }
 }
